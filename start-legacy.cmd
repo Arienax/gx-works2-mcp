445a4bf1c9bccb796b5314333a6c5a88b9b0c3ca
Arienax <1307420856@qq.com>
2026-09-12 00:38:43 +0800

    chore: remove obsolete test and context release artifacts

--- start-legacy.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-chcp 65001 >nul
-title GXWorks Context Test - legacy
-powershell.exe -NoProfile -STA -ExecutionPolicy Bypass -File "%~dp0scripts\start_context_test.ps1" -Policy legacy
-if errorlevel 1 pause
-endlocal
